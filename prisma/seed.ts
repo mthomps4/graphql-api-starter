7eb62aba5608bfae25a54a2004e762b745ce9099
Matt Thompson <matt@echobind.com>
2021-12-01 10:56:22 -0500

    remove react-apollo types from codegen

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,9 @@
+import { roleSeedDate, seedRoles } from './seeds/roles';
 import { userSeedData, seedUsers } from './seeds/users';
 
 const seed = async () => {
+  console.log('seeding Users...');
+  await seedRoles(roleSeedDate);
   console.log('seeding Users...');
   await seedUsers(userSeedData);
 };
